1593604cd7a14fc29124cbd98aab4e41dc635590
gurjeetsinghvirdee <gurjeetsinghvirdee@gmail.com>
2021-06-27 15:41:58 +0530

    created admin route

--- Backend/utils.js
+++ Backend/utils.js
@@ -31,7 +31,17 @@ module.exports = {
       } else {
       res.status(401).send({ message: 'No Token' });
       }
+  },
+
+  isAdmin : (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+      next();
+    } else {
+      res.status(401).send({ message: 'Invalid Admin Token' });
+    }
   }
 }
+
+
 
 
