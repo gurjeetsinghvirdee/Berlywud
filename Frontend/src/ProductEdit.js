f468725a000440cc1d757c0ea143826336c2078c
gurjeetsinghvirdee <gurjeetsinghvirdee@gmail.com>
2021-06-28 23:56:43 +0530

    creating some functions for editing

--- Frontend/src/ProductEdit.js
+++ Frontend/src/ProductEdit.js
@@ -32,7 +32,7 @@ export default function ProductEdit(props) {
   const [launch, setLaunch] = useState('');
   const [concentration, setConcentration] = useState('');
   const [rating, setRating] = useState('');
-  const [reviews, setReviews] = useState('');
+  const [review, setReview] = useState('');
   const [stockcount, setStockcount] = useState('');
   const [twoml,setTwoml] = useState('');
   const [fiveml,setFiveml] = useState('');
@@ -42,9 +42,9 @@ export default function ProductEdit(props) {
   const [topnote1, setTopnote1] = useState('');
   const [topnote2, setTopnote2] = useState('');
   const [topnote3, setTopnote3] = useState('');
-  const [midddlenote1, setMiddlenote1] = useState('');
-  const [midddlenote2, setMiddlenote2] = useState('');
-  const [midddlenote3, setMiddlenote3] = useState('');
+  const [middlenote1, setMiddlenote1] = useState('');
+  const [middlenote2, setMiddlenote2] = useState('');
+  const [middlenote3, setMiddlenote3] = useState('');
   const [basenote1, setBasenote1] = useState('');
   const [basenote2, setBasenote2] = useState('');
   const [basenote3, setBasenote3] = useState('');
@@ -65,7 +65,7 @@ export default function ProductEdit(props) {
         setLaunch(product.launch);
         setConcentration(product.concentration);
         setRating(product.rating);
-        setReviews(product.reviews);
+        setReview(product.review);
         setStockcount(product.stockcount);
         setTwoml(product.decantprice["2ml"])
         setFiveml(product.decantprice["5ml"])
@@ -146,6 +146,15 @@ export default function ProductEdit(props) {
             />
             <TextField 
                 id="outlined-basic"
+                required
+                variant="outlined"
+                label="Stock Count"
+                value={stockcount}
+                color="secondary"
+                onChange={(e) => setStockcount(e.target.value)}
+            />
+            <TextField 
+                id="outlined-basic"
                 fullWidth
                 required
                 variant="outlined" 
@@ -153,16 +162,6 @@ export default function ProductEdit(props) {
                 value={url}
                 color="secondary"
                 onChange={(e) => setUrl(e.target.value)}
-            />
-            <br/>
-             <TextField 
-                id="outlined-basic"
-                required
-                variant="outlined"
-                label="Retail Discounted Price"
-                value={origprice}
-                color="secondary"
-                onChange={(e) => setOrigprice(e.target.value)}
             />
             <TextField 
                 id="outlined-multiline-static"
@@ -176,12 +175,179 @@ export default function ProductEdit(props) {
                 onChange={(e) => setDescription(e.target.value)}
             />
             
-            {/* <div>
-              <label></label>
-              <button className="primary" type="submit">
-                Update
-              </button>
-            </div> */}
+            <div>
+              <h3>Notes</h3>
+            </div>
+
+            <TextField 
+                id="outlined-basic"
+                required
+                variant="outlined"
+                label="Top Note 1"
+                value={topnote1}
+                color="secondary"
+                onChange={(e) => setTopnote1(e.target.value)}
+            />
+            <TextField 
+                id="outlined-basic"
+                required
+                variant="outlined"
+                label="Top Note 2"
+                value={topnote2}
+                color="secondary"
+                onChange={(e) => setTopnote2(e.target.value)}
+            />
+            <TextField 
+                id="outlined-basic"
+                required
+                variant="outlined"
+                label="Top Note 3"
+                value={topnote3}
+                color="secondary"
+                onChange={(e) => setTopnote3(e.target.value)}
+            />
+
+            <br/>
+
+            <TextField 
+                id="outlined-basic"
+                required
+                variant="outlined"
+                label="Middle Note 1"
+                value={middlenote1}
+                color="secondary"
+                onChange={(e) => setMiddlenote1(e.target.value)}
+            />
+            <TextField 
+                id="outlined-basic"
+                required
+                variant="outlined"
+                label="Middle Note 2"
+                value={middlenote2}
+                color="secondary"
+                onChange={(e) => setMiddlenote2(e.target.value)}
+            />
+            <TextField 
+                id="outlined-basic"
+                required
+                variant="outlined"
+                label="Middle Note 3"
+                value={middlenote3}
+                color="secondary"
+                onChange={(e) => setMiddlenote3(e.target.value)}
+            />
+
+            <br/>
+
+            <TextField 
+                id="outlined-basic"
+                required
+                variant="outlined"
+                label="Base Note 1"
+                value={basenote1}
+                color="secondary"
+                onChange={(e) => setBasenote1(e.target.value)}
+            />
+            <TextField 
+                id="outlined-basic"
+                required
+                variant="outlined"
+                label="Base Note 2"
+                value={basenote2}
+                color="secondary"
+                onChange={(e) => setBasenote2(e.target.value)}
+            />
+            <TextField 
+                id="outlined-basic"
+                required
+                variant="outlined"
+                label="Base Note 3"
+                value={basenote3}
+                color="secondary"
+                onChange={(e) => setBasenote3(e.target.value)}
+            />
+            <div>
+              <h3>Price</h3>
+            </div>
+
+            <TextField 
+                id="outlined-basic"
+                required
+                variant="outlined"
+                label="Retail Discounted Price"
+                value={retail}
+                color="secondary"
+                onChange={(e) => setRetail(e.target.value)}
+            />
+            <TextField 
+                id="outlined-basic"
+                required
+                variant="outlined"
+                label="2ml Price"
+                value={twoml}
+                color="secondary"
+                onChange={(e) => setTwoml(e.target.value)}
+            />
+            <TextField 
+                id="outlined-basic"
+                required
+                variant="outlined"
+                label="5ml Price"
+                value={fiveml}
+                color="secondary"
+                onChange={(e) => setFiveml(e.target.value)}
+            />
+            <TextField 
+                id="outlined-basic"
+                required
+                variant="outlined"
+                label="10ml Price"
+                value={tenml}
+                color="secondary"
+                onChange={(e) => setTenml(e.target.value)}
+            />
+            <TextField 
+                id="outlined-basic"
+                required
+                variant="outlined"
+                label="30ml Price"
+                value={thirtyml}
+                color="secondary"
+                onChange={(e) => setThirtyml(e.target.value)}
+            />
+            <TextField 
+                id="outlined-basic"
+                required
+                variant="outlined"
+                label="Retail Price"
+                value={origprice}
+                color="secondary"
+                onChange={(e) => setOrigprice(e.target.value)}
+            />
+            <div>
+              <h3>Rating</h3>
+            </div>
+            <TextField 
+                id="outlined-basic"
+                required
+                variant="outlined"
+                label="Rating"
+                value={rating}
+                color="secondary"
+                onChange={(e) => setRating(e.target.value)}
+            />
+            <div>
+              <h3>Review</h3>
+            </div>
+            <TextField 
+                id="outlined-basic"
+                required
+                variant="outlined"
+                label="Review"
+                value={review}
+                color="secondary"
+                onChange={(e) => setReview(e.target.value)}
+            />
           </>
         )}
       </form>
